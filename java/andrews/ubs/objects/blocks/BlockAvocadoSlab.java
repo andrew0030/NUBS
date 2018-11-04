abbc030c9f36f88b2b79d4b84f541ee17a140e76
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2018-11-04 22:04:10 +0200

    Finished Slabs
    Finally fixed all known issues with slabs.

--- java/andrews/ubs/objects/blocks/BlockAvocadoSlab.java
+++ java/andrews/ubs/objects/blocks/BlockAvocadoSlab.java
@@ -9,14 +9,22 @@ import net.minecraft.block.BlockSlab;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
+
+	
 public abstract class BlockAvocadoSlab extends BlockSlab
-{	
+{
+	public static final PropertyEnum<Variant> VARIANT = PropertyEnum.<Variant>create("variant", Variant.class);
+	
 	public BlockAvocadoSlab(String name)
 	{
 		super(Material.WOOD);
@@ -24,69 +32,92 @@ public abstract class BlockAvocadoSlab extends BlockSlab
 		this.setRegistryName(new ResourceLocation(Reference.MODID, name));
 		this.setSoundType(SoundType.WOOD);
 		this.setHardness(2F);
-		IBlockState state = this.blockState.getBaseState();
+		this.useNeighborBrightness = !this.isDouble();
+		this.setCreativeTab(Main.instance.blocktab);
+		
+		IBlockState state = this.blockState.getBaseState().withProperty(VARIANT, Variant.DEFAULT);
 		if(!this.isDouble())
 		{
-			state = state.withProperty(HALF, EnumBlockHalf.BOTTOM);
+			state.withProperty(HALF, BlockSlab.EnumBlockHalf.BOTTOM);
 		}
-		setDefaultState(state);
-		this.useNeighborBrightness = true;
-		this.setCreativeTab(Main.instance.blocktab);
-		this.getStateFromMeta(1);
+		this.setDefaultState(state);
 		
 		BlockInit.BLOCKS.add(this);
 	}
-	
+
 	@Override
 	public String getUnlocalizedName(int meta)
 	{
-		return this.getUnlocalizedName();
+		return super.getUnlocalizedName();
 	}
-	
+
 	@Override
 	public IProperty<?> getVariantProperty()
 	{
-		return HALF;
+		return VARIANT;
 	}
-	
+
 	@Override
 	public Comparable<?> getTypeForItem(ItemStack stack)
 	{
-		return EnumBlockHalf.BOTTOM;
+		return Variant.DEFAULT;
 	}
-	
+
 	@Override
-	public int damageDropped(IBlockState state) 
-	{
-		return 0;
-	}
-	
-	@Override
-	public IBlockState getStateFromMeta(int meta)
-	{
-		if(!this.isDouble())
-			return this.getDefaultState().withProperty(HALF, EnumBlockHalf.values()[meta % EnumBlockHalf.values().length]);
-		return this.getDefaultState();
-	}
-	
-	@Override
-	public int getMetaFromState(IBlockState state)
-	{
-		if(this.isDouble())
-			return 0;
-		return ((EnumBlockHalf)state.getValue(HALF)).ordinal() + 1;
-	}
-	
-	@Override
-	public Item getItemDropped(IBlockState state, Random rand, int fortune) 
+	public Item getItemDropped(IBlockState state, Random rand, int fortune)
 	{
 		return Item.getItemFromBlock(BlockInit.AVOCADO_SLAB_HALF);
+	}
+	
+	
+	@Override
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
+	{
+		return new ItemStack(BlockInit.AVOCADO_SLAB_HALF);
+	}
+	
+	@Override
+	public final IBlockState getStateFromMeta(final int meta)
+	{
+		IBlockState blockstate = this.blockState.getBaseState().withProperty(VARIANT, Variant.DEFAULT);
+		
+		if(!this.isDouble())
+		{
+			blockstate = blockstate.withProperty(HALF, ((meta&8)!=0)?EnumBlockHalf.TOP:EnumBlockHalf.BOTTOM);
+		}
+		return blockstate;
+	}
+	
+	@Override
+	public final int getMetaFromState(final IBlockState state)
+	{
+		int meta = 0;
+		
+		if(!this.isDouble()&& state.getValue(HALF)==EnumBlockHalf.TOP)
+		{
+			meta |= 8;
+		}
+		return meta;
 	}
 	
 	@Override
 	protected BlockStateContainer createBlockState()
 	{
-		return new BlockStateContainer(this, new IProperty[] {HALF});
+		if(!this.isDouble())
+		{
+			return new BlockStateContainer(this, new IProperty[] {VARIANT, HALF});
+		}
+		return new BlockStateContainer(this, new IProperty[] {VARIANT});
 	}
 	
+	public static enum Variant implements IStringSerializable
+	{
+		DEFAULT;
+
+		@Override
+		public String getName() 
+		{
+			return "default";
+		}
+	}
 }
